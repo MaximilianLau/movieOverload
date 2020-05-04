ee3efd82165ed0ade404b00247370e73ed285a06
MaximilianLau <43243180+MaximilianLau@users.noreply.github.com>
2020-05-04 09:46:33 -0400

    button styling went wonky； fixed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
         <footer className="wrapper">
-            <p>Copyright &copy; 2020 <a href="http://maximilianlau.com" target="_blank" rel="noopener noreferrer">Maximilian Lau.</a></p>
+            <p>&copy; 2020 <a href="http://maximilianlau.com" target="_blank" rel="noopener noreferrer">Maximilian Lau.</a></p>
             <p>Powered by React, The Movies DB, and OMDB</p>
         </footer>
     )
