b351769c3d14950d19ef9757fce1735101cae60a
MaximilianLau <43243180+MaximilianLau@users.noreply.github.com>
2020-05-05 14:36:44 -0400

    deployed old

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
-    return (
-        <footer className="wrapper">
-            <p>&copy; 2020 <a href="http://maximilianlau.com" target="_blank" rel="noopener noreferrer">Maximilian Lau.</a></p>
-            <p>Powered by React, The Movies DB, and OMDB</p>
-        </footer>
-    )
-  }
+function Footer() {
+  return (
+    <footer className="wrapper">
+      <p>&copy; 2020 <a href="http://maximilianlau.com" target="_blank" rel="noopener noreferrer">Maximilian Lau.</a></p>
+      <p>Powered by React, The Movies DB, and OMDB</p>
+    </footer>
+  )
 }
 
 export default Footer;
